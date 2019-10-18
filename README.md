c08a837443b64d5cc901934e01823a4cdd983486
Pascal Fischer <pascal.fischer@fondof.de>
2019-10-18 09:16:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Extends order with newsletter signup
+[![Build Status](https://travis-ci.org/fond-of/spryker-category.svg?branch=master)](https://travis-ci.org/fond-of-spryker/sales-newsletter-signup)
+[![PHP from Travis config](https://img.shields.io/travis/php-v/symfony/symfony.svg)](https://php.net/)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://packagist.org/packages/fond-of-spryker/sales-newsletter-signup)
+
+## Installation
+
+```
+composer require fond-of-spryker/sales-newsletter-signup
+```
---
+++ src/FondOfSpryker/Zed/SalesNewsletterSignup/Communication/Plugin/Sales/NewsletterSignupOrderExpanderPlugin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FondOfSpryker\Zed\SalesNewsletterSignup\Communication\Plugin\Sales;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\SpySalesOrderEntityTransfer;
+use Spryker\Zed\Sales\Dependency\Plugin\OrderExpanderPreSavePluginInterface;
+
+class NewsletterSignupOrderExpanderPlugin implements OrderExpanderPreSavePluginInterface
+{
+    /**
+     * Specification:
+     *   - Its a plugin which hydrates SpySalesOrderEntityTransfer before order created
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\SpySalesOrderEntityTransfer $spySalesOrderEntityTransfer
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\SpySalesOrderEntityTransfer
+     */
+    public function expand(SpySalesOrderEntityTransfer $spySalesOrderEntityTransfer, QuoteTransfer $quoteTransfer): SpySalesOrderEntityTransfer
+    {
+        $spySalesOrderEntityTransfer->setSignupNewsletter($quoteTransfer->getSignupNewsletter());
+
+        return $spySalesOrderEntityTransfer;
+    }
+}
